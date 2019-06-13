a29519a9aede19cd9fbb7e803cf32afd0b04996a
Offek Gil <Offekg@gmail.com>
2019-06-13 12:24:26 +0300

    started cell and board structs
    started cell and board structs

--- game.c
+++ game.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "game.h"
 #include "parser.h"
 #include "solver.h"
+
+/*
+ * fixed == 1 means cell is fixed; ==0 means cell is not fixed
+ */
+Cell* createCell(int val, int fixed){
+	Cell* cell = (Cell*) malloc(sizeof(Cell));
+	cell->value = val;
+	if(fixed == 1)
+		cell->isFixed = 1;
+	else
+		cell->isFixed = 0;
+	return cell;
+}
+
+void destroyCell(Cell* cell){
+	if(!cell)
+		return;
+	free(cell);
+}
 
 void initializeBoard(){
 	int fixedCells;
--- game.h
+++ game.h
@@ -1,1 +1,34 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+typedef struct cell_t{
+	int value;
+	int isFixed;
+
+} Cell;
+
+Cell* createCell(int val, int fixed);
+void destroyCell(Cell* cell);
+
+typdef struct board_t{
+	Cell** current_board;
+	int solution[][];
+	int board_size;
+	int block_size;
+} Board;
+
+
+Board* createBoard(int board_size, int block_size);
+void destroyBoard(Board* b);
+
 void initializeBoard();
+
+
+
+
+
+
+
+
+
+#endif
