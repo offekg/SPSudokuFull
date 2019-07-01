67c54b696ad47e8c42e143d16691cd7b7ab68667
Offek Gil <Offekg@gmail.com>
2019-07-01 13:52:15 +0300

    cleaned main

--- main.c
+++ main.c
@@ -11,36 +11,6 @@
 
 #define MAX_COMMAND_SIZE 1026
 
-int game_runner(){
-	Command* command;
-	char userInput[MAX_COMMAND_SIZE] = { 0 };
-	Board* board;
-	board = create_blank_board(9,3,3);
-	generate_user_board(board);
-	printBoard(board,0);
-
-	while(1){
-		if (fgets(userInput, MAX_COMMAND_SIZE, stdin) == NULL) {
-			if (ferror(stdin)) {
-				printf("Error: fgets has failed\n");
-			}
-			return 0;
-		}
-
-
-		command = parse_command(userInput);
-		if (!command) {
-			/*
-			 * Could not parse a legible command, skipping.
-			 */
-			continue;
-		}
-		execute_command(command, board);
-	}
-	destroyBoard(board);
-	return 0;
-}
-
 
 int main(int argc, char *argv[]){
 	Command* command;
